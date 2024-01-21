386b1c54cd494667bedbfa02cdf5f416807cff57
chrisadams <chris.adams@hinge.co>
2024-01-21 00:09:34 -0500

    Update RulesModal.jsx

--- components/modals/RulesModal.jsx
+++ components/modals/RulesModal.jsx
@@ -15,10 +15,10 @@ export default function RulesModal(props) {
             <ul>
                 <li>You need at least 4 people to play this game</li>
                 <li>The game is played in rounds where one person is the dasher and the others are guessers</li>
-                <li>The dasher selects and submits an obscure word which the others are either trying to guess or come up with a realistic definition for</li>
-                <li>Once everyone submits their answers, the dasher reviews them and marks some as correct while all the other definitions together with a single correct one are submitted for voting</li>
-                <li>The guessers vote for the definition they think is correct</li>
-                <li>The scores are awarded in the end of each round: if no one guesses the correct word, the dasher gets 2 points, players who correctly define the word get a point and players who come up with a false definition get a single point for each vote they receive</li>
+                <li>The dasher selects and submits an obscure prompt which the others are either trying to guess or come up with a realistic answer for</li>
+                <li>Once everyone submits their answers, the dasher reviews them and marks any that are correct while all the other definitions together with a single correct one are submitted for voting</li>
+                <li>The guessers vote for the answer they think is the real one</li>
+                <li>The scores are awarded in the end of each round: if no one guesses the correct answer, the dasher gets 2 points, players who correctly anwer get a point and players who come up with a false answer get a single point for each vote they receive</li>
                 <li>The player with the highest score wins!</li>
             </ul>
 
